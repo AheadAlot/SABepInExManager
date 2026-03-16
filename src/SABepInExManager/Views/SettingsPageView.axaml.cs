3ab9f09b24f8c137deed2b159a20f068e147259d
AheadAlot <92690997+AheadAlot@users.noreply.github.com>
2026-03-16 00:10:05 -0700

    ui: 设置界面恢复路径选择文件夹和自动探测按钮

--- src/SABepInExManager/Views/SettingsPageView.axaml.cs
+++ src/SABepInExManager/Views/SettingsPageView.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using SABepInExManager.ViewModels;
+using System.Linq;
 
 namespace SABepInExManager.Views;
 
@@ -7,6 +10,58 @@ public partial class SettingsPageView : UserControl
     public SettingsPageView()
     {
         InitializeComponent();
+    }
+
+    private async void OnPickGameRootFolderClicked(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (DataContext is not SettingsPageViewModel vm)
+        {
+            return;
+        }
+
+        var storageProvider = TopLevel.GetTopLevel(this)?.StorageProvider;
+        if (storageProvider is null)
+        {
+            return;
+        }
+
+        var folders = await storageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
+        {
+            Title = "选择游戏根目录",
+            AllowMultiple = false,
+        });
+
+        var folder = folders.FirstOrDefault();
+        if (folder is not null)
+        {
+            vm.HomePage.GameRootPath = folder.Path.LocalPath;
+        }
+    }
+
+    private async void OnPickWorkshopFolderClicked(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (DataContext is not SettingsPageViewModel vm)
+        {
+            return;
+        }
+
+        var storageProvider = TopLevel.GetTopLevel(this)?.StorageProvider;
+        if (storageProvider is null)
+        {
+            return;
+        }
+
+        var folders = await storageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
+        {
+            Title = "选择创意工坊路径",
+            AllowMultiple = false,
+        });
+
+        var folder = folders.FirstOrDefault();
+        if (folder is not null)
+        {
+            vm.HomePage.WorkshopContentPath = folder.Path.LocalPath;
+        }
     }
 }
 
